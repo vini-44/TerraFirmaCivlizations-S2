4f132a1ba74b69c1d41913b4202f494f859edc84
Daniel <binaryclock03@gmail.com>
2025-11-02 14:55:23 -0500

    added script to make some blocks like vents and all vanilla blocks unbreakable in survival

---
+++ overrides/kubejs/server_scripts/src/make_unbreakable.js
@@ -0,0 +1,21 @@
+//make all minecraft blocks unbreakable except in creative mode
+BlockEvents.broken(event => {
+  if (event.getPlayer().isCreative()) return
+  if (event.block.id.split(':')[0] == 'minecraft') event.cancel()
+})
+
+
+//list of unbreakable blocks
+UNBREAKABLE_BLOCKS = [
+  'create_resource_vents:active_mantle_copper_vent',
+  'create_resource_vents:active_mantle_iron_vent',
+  'create_resource_vents:active_mantle_tin_vent',
+  'create_resource_vents:active_mantle_gold_vent'
+]
+
+for (let block of UNBREAKABLE_BLOCKS) {
+  BlockEvents.broken(block, event => {
+    if (event.getPlayer().isCreative()) return
+    event.cancel()
+  })
+}
