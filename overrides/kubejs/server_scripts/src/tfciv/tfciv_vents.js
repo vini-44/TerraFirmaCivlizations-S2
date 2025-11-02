7cf285d3225c948d056ab647cf5ab03aeea70e1c
Vini <viniclover44@gmail.com>
2025-11-02 00:08:20 -0700

    starting adding vents crafting recipes

--- overrides/kubejs/server_scripts/src/tfciv/tfciv_vents.js
+++ overrides/kubejs/server_scripts/src/tfciv/tfciv_vents.js
@@ -1,4 +1,14 @@
 ServerEvents.recipes((event) => {  
-	event.recipes.create.crushing(['tfc:ore/normal_native_copper', Item.of('kubejs:copper_mantle_ore', 0.5)], 'kubejs:copper_mantle_ore')
-	event.recipes.create.milling(['tfc:ore/normal_native_copper', Item.of('kubejs:copper_mantle_ore', 0.5)], 'kubejs:copper_mantle_ore')
+	event.recipes.create.crushing(
+		[
+			'tfc:ore/normal_native_copper', Item.of('tfc:ore/normal_native_copper').withChance(0.25)
+		], 
+		'kubejs:copper_mantle_ore'
+	).processingTime(500)
+	event.recipes.create.milling(
+		[
+			'tfc:ore/normal_native_copper', Item.of('tfc:ore/normal_native_copper').withChance(0.25)
+		], 
+		'kubejs:copper_mantle_ore'
+	).processingTime(1000)
 });
