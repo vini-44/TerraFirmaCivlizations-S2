628b4f5b972b02d6d78d9cbeb433c7d23e71625b
Vini <viniclover44@gmail.com>
2025-11-06 23:55:24 -0800

    add nether bricks to unbreakable blocks just in case

--- overrides/kubejs/server_scripts/src/tfciv/make_unbreakable.js
+++ overrides/kubejs/server_scripts/src/tfciv/make_unbreakable.js
@@ -3,7 +3,8 @@ const UNBREAKABLE_BLOCKS = [
   'create_resource_vents:active_mantle_copper_vent',
   'create_resource_vents:active_mantle_iron_vent',
   'create_resource_vents:active_mantle_tin_vent',
-  'create_resource_vents:active_mantle_gold_vent'
+  'create_resource_vents:active_mantle_gold_vent',
+  'minecraft:nether_bricks'
 ]
 
 for (let block of UNBREAKABLE_BLOCKS) {
