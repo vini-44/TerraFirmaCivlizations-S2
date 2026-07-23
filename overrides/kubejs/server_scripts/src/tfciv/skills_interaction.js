27a9ab1da86774af17917d1fefa39f01abd2e964
Vini <viniclover44@gmail.com>
2026-07-23 05:59:54 -0700

    flesh out miner tree, tweak coldsweat, remove piligars, new powers

--- overrides/kubejs/server_scripts/src/tfciv/skills_interaction.js
+++ overrides/kubejs/server_scripts/src/tfciv/skills_interaction.js
@@ -6,9 +6,13 @@ BlockEvents.rightClicked(event => {
 	leatherCheck(event);
 	meatHookCheck(event);
 	cleaverRightClickCheck(event);
+	proPickCheck(event);
 });
 BlockEvents.broken(event => {
 	cropCheck(event);
+	oreCheck(event);
+	depthSicknessCheck(event);
+	fluxboreCheck(event);
 });
 EntityEvents.hurt(event => {
 	cleaverDamageCheck(event);
@@ -58,50 +62,67 @@ LootJS.modifiers(event => {
         });
 });
 
-function animalDropCheck(event) {
-	const { entity, source, drops, level } = event;
-
-	// Only care about kills by a player
-	const attacker = source.actual;
+//right click checks
+function fruitCheck(event) {
+	const { player, level, hand, block} = event;
 
 	
-	if (!attacker.isPlayer()) return;
+	if (hand !== 'main_hand') return;
+	if (!block.hasTag('tfc:fruit_tree_leaves')) return;
+  	if (block.properties.lifecycle !== 'fruiting') return;
 
-	if (!attacker.tags.contains('animal_bonus_1')) return;
+	//for (const key in player) {
+	//	console.log(key);
+	//}
 
-	//attacker.tell(`kill by ${attacker}, ${attacker.tags}`);
+	//console.log(`lifecycle value: "${block.properties.lifecycle}"`);
+	//player.tell(`lifecycle value: "${block.properties.lifecycle}"`);
 
-	let chance = 1;
+	const playerName = (String(player.name).split('{')[1]).split('}')[0];
+	
+	SkillExperience.addExperience(player, "puffish_skills:farmer", 5);
 
-	if (attacker.tags.contains('animal_bonus_1')) chance += 0.10;
-	if (attacker.tags.contains('animal_bonus_2')) chance += 0.10;
-	if (attacker.tags.contains('animal_bonus_3')) chance += 0.15;
-	if (attacker.tags.contains('animal_bonus_4')) chance += 0.15;
-
-	//attacker.tell(`chance: ${chance}`);
+	if (!player.tags.contains('fruit_bonus_1')) return;
+	
+	let chance = 0;
+	if (player.tags.contains('orchardist')) chance += 0.15;
+	if (player.tags.contains('fruit_bonus_1')) chance += 0.15;
+	if (player.tags.contains('fruit_bonus_2')) chance += 0.15;
+	if (player.tags.contains('fruit_bonus_3')) chance += 0.10;
+	if (player.tags.contains('fruit_bonus_4')) chance += 0.10;
 
 	if (Math.random() > chance) return;
 
-    const originalItems = [];
-    for (let i = 0; i < drops.size(); i++) {
-        originalItems.push(drops.get(i).item); // pull the ItemStack out of the ItemEntity
-    }
+	SkillExperience.addExperience(player, "puffish_skills:farmer", 5);
 
-    originalItems.forEach(stack => {
-		attacker.tell(Text.of(`Bonus drop! ${stack.count}x `).append(stack.getDisplayName()));
-        event.addDrop(stack.copy());
-    });
+	const blockId = String(block).split('[')[0];
+	const fruitName = blockId.split('/')[1].replace('_leaves', '');
+	const foodId = `tfc:food/${fruitName}`;
+
+	//player.tell('You got an extra fruit! ' + foodId);
+	player.give(Item.of(foodId, 1));
 }
-function cleaverDamageCheck(event) {
-	const {source} = event;
-	const player = source.actual;
+function jamCheck(event) {
+	const { player, level, hand} = event;
 
-	if(!player) return;
+	if (hand !== 'main_hand') return;
 
-	if(!player.tags || !player.mainHandItem) return;
+	if(player.mainHandItem.id == 'tfc:empty_jar' && !player.tags.contains('orchardist')) {
+		player.tell('You need to be an orchardist to collect jam!');
+		event.cancel();
+	}
+	else if (player.mainHandItem.id == 'tfc:empty_jar' && player.tags.contains('orchardist')){
+		SkillExperience.addExperience(player, "puffish_skills:farmer", 10);
+	}
+}
+function ovenCheck(event) {
+	const { player, level, hand, block} = event;
 
-	if(!player.tags.contains('butcher') && player.mainHandItem.id === 'butchersdelight:cleaver') {
-		player.tell('You need to be a butcher to use the cleaver!');
+	if (hand !== 'main_hand') return;
+	if (!block.hasTag('firmalife:oven_blocks')) return;
+
+	if(!player.tags.contains('grand_farmer')) {
+		player.tell('You need to be a grand farmer to use the oven!');
 		event.cancel();
 	}
 }
@@ -139,27 +160,48 @@ function leatherCheck(event) {
 		event.cancel();
 	}
 }
-function ovenCheck(event) {
-	const { player, level, hand, block} = event;
-
-	if (hand !== 'main_hand') return;
-	if (!block.hasTag('firmalife:oven_blocks')) return;
-
-	if(!player.tags.contains('grand_farmer')) {
-		player.tell('You need to be a grand farmer to use the oven!');
-		event.cancel();
-	}
-}
-function jamCheck(event) {
-	const { player, level, hand} = event;
+function proPickCheck(event) {
+	const {player, hand} = event;
 
 	if (hand !== 'main_hand') return;
 
-	if(player.mainHandItem.id == 'tfc:empty_jar' && !player.tags.contains('orchardist')) {
-		player.tell('You need to be an orchardist to collect jam!');
+	if(!player.tags.contains('spelunker') && (
+		player.mainHandItem.hasTag('tfc:propicks') ||
+		player.mainHandItem.hasTag('precisionprospecting:prospector_hammers') ||
+		player.mainHandItem.hasTag('precisionprospecting:prospector_drills') ||
+		player.mainHandItem.hasTag('precisionprospecting:mineral_prospectors') 
+	))
+	{
+		player.tell(`You need to be a Spelunker to use this tool!`);
+		event.cancel();
+	}
+	else if(player.tags.contains('spelunker') && (
+		player.mainHandItem.hasTag('tfc:propicks') ||
+		player.mainHandItem.hasTag('precisionprospecting:prospector_hammers') ||
+		player.mainHandItem.hasTag('precisionprospecting:prospector_drills') ||
+		player.mainHandItem.hasTag('precisionprospecting:mineral_prospectors') 
+	))
+	{
+		SkillExperience.addExperience(player, "puffish_skills:miner", 1);
+	}
+}
+
+//entity hurt checks
+function cleaverDamageCheck(event) {
+	const {source} = event;
+	const player = source.actual;
+
+	if(!player) return;
+
+	if(!player.tags || !player.mainHandItem) return;
+
+	if(!player.tags.contains('butcher') && player.mainHandItem.id === 'butchersdelight:cleaver') {
+		player.tell('You need to be a butcher to use the cleaver!');
 		event.cancel();
 	}
 }
+
+// Block break events
 function cropCheck(event) {
 	const { player, level, hand, block} = event;
 
@@ -181,44 +223,67 @@ function cropCheck(event) {
 		event.block.popItem(drop);
 	});
 	
+	SkillExperience.addExperience(player, "puffish_skills:farmer", 5)
+
 }
-function fruitCheck(event) {
-	const { player, level, hand, block} = event;
+function oreCheck(event) {
+	const { player, level, block} = event;
 
-	
-	if (hand !== 'main_hand') return;
-	if (!block.hasTag('tfc:fruit_tree_leaves')) return;
-  	if (block.properties.lifecycle !== 'fruiting') return;
-
-	//for (const key in player) {
-	//	console.log(key);
-	//}
-
-	//console.log(`lifecycle value: "${block.properties.lifecycle}"`);
-	//player.tell(`lifecycle value: "${block.properties.lifecycle}"`);
-
-	const playerName = (String(player.name).split('{')[1]).split('}')[0];
-	
-	event.server.runCommandSilent(`puffish_skills experience add ${playerName} puffish_skills:farmer 10`);
-
-
-	if (!player.tags.contains('fruit_bonus_1')) return;
+	if (!block.hasTag('forge:ores') && !block.hasTag('terrafirmacivilizations:mantle_ore')) return;
 	
 	let chance = 0;
-	if (player.tags.contains('orchardist')) chance += 0.15;
-	if (player.tags.contains('fruit_bonus_1')) chance += 0.15;
-	if (player.tags.contains('fruit_bonus_2')) chance += 0.15;
-	if (player.tags.contains('fruit_bonus_3')) chance += 0.10;
-	if (player.tags.contains('fruit_bonus_4')) chance += 0.10;
+	if (player.tags.contains('mining_bonus_1')) chance += 0.05;
+	if (player.tags.contains('mining_bonus_2')) chance += 0.05;
+	if (player.tags.contains('mining_bonus_3')) chance += 0.10;
+	if (player.tags.contains('mining_bonus_4')) chance += 0.10;
+	if (player.tags.contains('mining_bonus_5')) chance += 0.10;
+	if (player.tags.contains('mining_bonus_6')) chance += 0.10;
 
 	if (Math.random() > chance) return;
+	let normalDrops = block.getDrops();
 
-	event.server.runCommandSilent(`puffish_skills experience add ${playerName} puffish_skills:farmer 10`);
+	// Spawn a duplicate of each dropped item into the world
+	normalDrops.forEach(drop => {
+		event.block.popItem(drop);
+	});
 
-	const blockId = String(block).split('[')[0];
-	const fruitName = blockId.split('/')[1].replace('_leaves', '');
-	const foodId = `tfc:food/${fruitName}`;
+	SkillExperience.addExperience(player, "puffish_skills:miner", 10)
+}
+function depthSicknessCheck(event){
+	const { player, level, hand, block} = event;
 
-	//player.tell('You got an extra fruit! ' + foodId);
-	player.give(Item.of(foodId, 1));
+	if (player.y >= 55) return;	
+	
+	let lowestDepth = 55;
+	if (player.tags.contains('depth_sickness_1')) lowestDepth -= 10;
+	if (player.tags.contains('depth_sickness_2')) lowestDepth -= 20;
+	if (player.tags.contains('depth_sickness_3')) lowestDepth -= 20;
+	if (player.tags.contains('depth_sickness_4')) lowestDepth -= 20;
+	if (player.tags.contains('depth_sickness_5')) lowestDepth -= 25;
+
+	if (player.y >= lowestDepth) return;
+	if (!player.tags.contains('depth_sickness_5')){
+		player.potionEffects.add('minecraft:slowness', 120 * 20, 0);
+		player.potionEffects.add('minecraft:hunger', 60 * 20, 1);
+		player.potionEffects.add('tfc:thirst', 60 * 20, 1);
+		player.potionEffects.add('minecraft:nausea', 5 * 20, 0);
+		player.potionEffects.add('minecraft:mining_fatigue', 5 * 20, 0);
+		player.potionEffects.add('minecraft:blindness', 120 * 20, 0);
+	}
+	else{	
+		player.potionEffects.add('minecraft:blindness', 20 * 20, 0);
+	}
+
+	player.tell(`You're in too deep, your safe depth is ${lowestDepth}, you're currently at ${player.y}`);
+
+	event.server.runCommandSilent('cave_dweller fast_forward minecraft:overworld 800 400');
+}
+function fluxboreCheck(event){
+	const { player, level, block} = event;
+	
+	if (!player.tags.contains('bulk_miner') && player.mainHandItem.id == 'thermal:flux_drill') {
+		player.tell("You need to be a Bulk Miner to use the Fluxbore");
+		event.cancel();
+	}
+		
 }
